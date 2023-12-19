26ad454176fce2829a225a3cebd44111164d1045
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-12-19 02:30:30 +0200

    Added main config 1.0

--- mail-automation-services/mailAutomation.service.py
+++ mail-automation-services/mailAutomation.service.py
@@ -1,10 +1,12 @@
-import config
 import time
-import email_content
-import app_password
+import fs
 import smtplib
 import ssl
 from email.message import EmailMessage
+
+#local imports
+import config
+
 #Define the progress bar function
 def progressBar(progress,total):
     percent=100*(progress/float(total))
@@ -12,14 +14,16 @@ def progressBar(progress,total):
     print(f"\r | {bar} | {percent:.2f}%",end='\r')
 
 
+#Define app_password
+app_password = config.APP_PASSWORD
+
 # Define email sender and receiver
 email_sender = config.SENDER_EMAIL_ADDRESS
-email_password = config.SENDER_PASSWORD
-receivers_emails = list(config.RECIEVER_EMAIL_ADDRESS)
+receivers_emails = list(config.RECIEVERS_EMAIL_ADDRESSES)
 
 # Set the subject and body of the email
-subject =email_content.subject
-body =email_content.body
+subject =config.EMAIL_SUBJECT
+if(config.EMAIL_BODY_TYPE=="html"):body =fs.open_fs(config.EMAIL_BODY_PATH)
 def send():
     progressBar(0,len(receivers_emails))
     for idx,receiver_email in enumerate(receivers_emails):
@@ -28,7 +32,9 @@ def send():
         em['From'] = email_sender
         em['To'] = receiver_email
         em['Subject'] = subject
-        em.set_content(body)
+
+        if(config.EMAIL_BODY_TYPE=="html"):em.add_alternative(body,subtype="html")
+        else:em.set_content(body)
 
         # Add SSL (layer of security)
         context = ssl.create_default_context()
@@ -36,7 +42,7 @@ def send():
         # Log in and send the email
         try:
             with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-                smtp.login(email_sender, app_password.gmail_app_password)
+                smtp.login(email_sender, app_password)
                 smtp.sendmail(email_sender, receiver_email, em.as_string())
             print("done")
         except:
