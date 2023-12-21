3fb6ca274ba55b2d9fc47eb05c79e72ca16cec67
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-12-21 23:31:47 +0200

    Added main controller 1.0

--- mainController.py
+++ mainController.py
@@ -1,4 +1,7 @@
 from taskFromInputServices import tasksFromCLI
 
+from tasksContentServices.tasksDict import tasks
+
 #get the tasks from the cli
-tasks=tasksFromCLI.getTasks()
+tasks=tasksFromCLI.getTasks(tasks)
+
