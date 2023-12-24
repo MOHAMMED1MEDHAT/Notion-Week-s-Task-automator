24a1f5ca224412749d2415b088d79da17a374472
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-12-24 05:01:58 +0200

    Added navigationFlow 1.0

--- services/taskAutomationServices/utils/navigationFlowUtil.py
+++ services/taskAutomationServices/utils/navigationFlowUtil.py
@@ -37,63 +37,19 @@ def addTask(driver,taskDate,task):
     pickDate_btn.click()
     #8 enter date dialog set_attribute("text","Dec 20,2023")
     setDate_dialog=WebDriverWait(driver,timeout=50).until(lambda el:el.find_element(By.XPATH,xpLocations.setDate_dialog_XP))
-    setDate_dialog.set_attribute("text",task)
+    setDate_dialog.set_attribute("text",taskDate)
     #9 click done btn
+    done_btn=WebDriverWait(driver,timeout=50).until(lambda el:el.find_element(By.XPATH,xpLocations.done_btn_XP))
+    done_btn.click()
     #10 click chooseType dropdownBtn
+    chooseType_dropdownBtn=WebDriverWait(driver,timeout=50).until(lambda el:el.find_element(By.XPATH,xpLocations.chooseType_dropdownBtn_XP))
+    chooseType_dropdownBtn.click()
     #11 click typeOption
+    typeOption=WebDriverWait(driver,timeout=50).until(lambda el:el.find_element(By.XPATH,xpLocations.typeOption_XP.format(taskType=task["type"])))
+    typeOption.click()
     #12 click save btn
-    #Dashboard--------------------------------------------------------
-    dashboard_a = WebDriverWait(driver, timeout=50).until(lambda el: el.find_element(By.XPATH,assests.DASHBOARD))
-    dashboard_a.click()
-
-    #access the event-----------------------------------------------------------
-    event_frame = WebDriverWait(driver, timeout=110).until(lambda el: el.find_element(By.XPATH,assests.EVENT))
-    event_frame.click()
-
-    #edit the event--------------------------------------------------------------
-    add_attendee_btn= WebDriverWait(driver, timeout=130).until(lambda el: el.find_element(By.XPATH,assests.ADD_ATTENDEE))
-    add_attendee_btn.click()
-
-    #add the attendees----------------------------------------------------------
-    def addAttendee(attendees):
-        assests.progressBar(0,len(attendees))
-        for idx,attendee in enumerate(attendees):
-            #insert the attendee first name
-            first_name=WebDriverWait(driver,timeout=150).until(lambda el:el.find_element(By.XPATH,assests.FIRSTNM))
-            first_name.send_keys(str(attendee["firstName"]))
-
-            #insert the attendee last name
-            last_name=WebDriverWait(driver,timeout=150).until(lambda el:el.find_element(By.XPATH,assests.LASTNM))
-            last_name.send_keys(str(attendee["lastName"]))
-
-            #insert the attendee mail
-            email=WebDriverWait(driver,timeout=150).until(lambda el:el.find_element(By.XPATH,assests.EMAIL))
-            email.send_keys(str(attendee["email"]))
-
-            #check if check in is marked
-            if(assests.CHECKIN_FLAG):
-                check_in=WebDriverWait(driver,timeout=150).until(lambda el:el.find_element(By.XPATH,assests.CHECKIN))
-                if(idx==0):
-                    check_in.click()
-                else:
-                    check_in.click()
-                    check_in.click()
-
-            #check if send event mail is marked
-            if(not(assests.SENDEMAL_FLAG)):
-                send_email=WebDriverWait(driver,timeout=150).until(lambda el:el.find_element(By.XPATH,assests.SENDMAIL))
-                send_email.click()
-
-
-            #check if it finished or not
-            if(idx==len(attendees)-1):
-                done=WebDriverWait(driver,timeout=160).until(lambda el:el.find_element(By.XPATH,assests.ADD))
-                done.click()
-            else:
-                more=WebDriverWait(driver,timeout=160).until(lambda el:el.find_element(By.XPATH,assests.SAVE_AND_CONT))
-                more.click()
-            time.sleep(2)
-            assests.progressBar(idx+1,len(attendees))
-
-    # addAttendee(attendees)
-
+    save_btn=WebDriverWait(driver,timeout=50).until(lambda el:el.find_element(By.XPATH,xpLocations.save_btn_XP))
+    save_btn.click()
+    #13 click create task btn
+    createTasks_btn = WebDriverWait(driver, timeout=50).until(lambda el: el.find_element(By.XPATH,xpLocations.createTasks_btn_XP))
+    createTasks_btn.click()
