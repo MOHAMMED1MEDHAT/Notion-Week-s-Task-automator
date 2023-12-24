eeb8b16837dad57fab2fbaf2f2776c82599239b6
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-12-24 05:15:47 +0200

    Added main controller v1 logic 1.0

--- mainController.py
+++ mainController.py
@@ -1,12 +1,16 @@
-from datetime import datetime
 
 #local imports
 from services.taskFromInputServices import tasksFromCLI
+from services.taskAutomationServices import taskCreationService
 from services.tasksContentServices.tasksDict import tasks
 
 if __name__=="__main__":
-    #get the tasks from the cli
+    #1) get the tasks from the cli
     tasks=tasksFromCLI.getTasks(tasks)
-    print(tasks)
-    # # print(datetime.fromisocalendar().strftime("%b %d,20%y"))
-    # print(datetime.now())
+    #2) add the tasks to notion
+    taskCreationService.createTasks(tasks)
+    #TODO:3) show a progress bar
+    #TODO:4) add a logger
+    #TODO:5) add mail service notification
+
+    
