4843935ca1a927f42387918c3d1d38d58cca820a
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-03-21 14:18:44 +0200

    chore(config):add config example

---
+++ config-example.py
@@ -0,0 +1,16 @@
+#mail settings
+APP_PASSWORD="your app password"
+SENDER_EMAIL_ADDRESS="your email address"
+RECIEVERS_EMAIL_ADDRESSES=["reciever email address"]
+
+EMAIL_SUBJECT="your weekly tasks had been made successfully"
+EMAIL_BODY="test body"
+EMAIL_BODY_TYPE="plain"
+EMAIL_BODY_PATH="./templates/standard.html"
+
+#chrome settings
+CHROME_DEBUGING_PORT='port number of your choice'
+NOTION_TASK_PAGE="your notion page link"
+
+#general settings
+TaskTypes=["different task types you want to add to your notion page"]
