7a0330d50589f8e4659af558dd917a5cc54b0262
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-03-29 23:17:08 +0200

    fix(driver):driver lunch issue

--- mainController.py
+++ mainController.py
@@ -13,4 +13,3 @@ if __name__=="__main__":
     #TODO:4) add a logger
     #TODO:5) add mail service notification
 
-    
--- services/taskAutomationServices/utils/selenuimIntiatorUtil.py
+++ services/taskAutomationServices/utils/selenuimIntiatorUtil.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -15,19 +17,22 @@ def startService():
     # subprocess.run("taskkill /F /IM \"chrome.exe\"", shell=True)
     # subprocess.run(f"chrome --remote-debugging-port={str(config.CHROME_DEBUGING_PORT)}", shell=True)
     subprocess.run("chrome --remote-debugging-port=9222", shell=True)
+    print("chrome is ready")
 
-    #2- connect to chrome
-    # chromeDebuggingServerLink=f"127.0.0.1:{str(config.CHROME_DEBUGING_PORT)}"
 
-    # chrome_options = Options()
-    # chrome_options.add_experimental_option("debuggerAddress", chromeDebuggingServerLink)
-    chrome_options = Options()
+    # #2- connect to chrome
+    # # chromeDebuggingServerLink=f"127.0.0.1:{str(config.CHROME_DEBUGING_PORT)}"
+
+    # # chrome_options = Options()
+    # # chrome_options.add_experimental_option("debuggerAddress", chromeDebuggingServerLink)
+
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
     chrome_driver = r"C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=chrome_driver, chrome_options=chrome_options)
+    print("driver is ready")
     driver.get(config.NOTION_TASK_PAGE)
 
-    return driver    
-
+    return driver
 def endService(driver):
     driver.quit()
